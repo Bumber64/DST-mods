a6e0ca06eadb1dc5ff905e881cb3f2934ddf30a0
Ryan Williams <ryan.williams.1990@gmail.com>
2022-07-25 18:44:46 -0700

    Create modinfo.lua

---
+++ Respawn Light/modinfo.lua
@@ -0,0 +1,14 @@
+
+name = "Respawn Light"
+description = "Creates a light source when players respawn, just like when joining the server."
+author = "Bumber"
+version = "1.1"
+forumthread = ""
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+api_version = 10
+dst_compatible = true
+all_clients_require_mod = false
+client_only_mod = false

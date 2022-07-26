7187263e60a9937b1796d076d79dc8a45c1bd0a5
Ryan Williams <ryan.williams.1990@gmail.com>
2022-07-25 18:56:44 -0700

    Create plantmeat_dried.lua

---
+++ Leaf Jerky/scripts/prefabs/plantmeat_dried.lua
@@ -0,0 +1,71 @@
+
+local assets =
+{
+    Asset("ANIM", "anim/plantmeat_dried.zip"),
+    Asset("ATLAS", "images/inventoryimages/plantmeat_dried.xml"),
+}
+
+local prefabs =
+{
+    "spoiled_food",
+}
+
+local function OnSpawnedFromHaunt(inst, data)
+    Launch(inst, data.haunter, TUNING.LAUNCH_SPEED_SMALL)
+end
+
+local function driedplantmeat()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddNetwork()
+
+    MakeInventoryPhysics(inst)
+
+    inst.AnimState:SetBank("plantmeat_dried")
+    inst.AnimState:SetBuild("plantmeat_dried")
+    inst.AnimState:PlayAnimation("idle_dried_plant")
+
+    inst:AddTag("meat")
+    inst:AddTag("dryable")
+    inst:AddTag("lureplant_bait")
+
+    MakeInventoryFloatable(inst)
+
+    inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+    inst:AddComponent("edible")
+    inst.components.edible.ismeat = true
+    inst.components.edible.foodtype = FOODTYPE.MEAT
+    inst.components.edible.healthvalue = TUNING.HEALING_MEDSMALL
+    inst.components.edible.hungervalue = TUNING.CALORIES_MEDSMALL
+    inst.components.edible.sanityvalue = TUNING.SANITY_SMALL
+
+    inst:AddComponent("bait")
+
+    inst:AddComponent("inspectable")
+
+    inst:AddComponent("inventoryitem")
+    inst.components.inventoryitem.atlasname = "images/inventoryimages/plantmeat_dried.xml"
+    inst:AddComponent("stackable")
+
+    inst:AddComponent("tradable")
+    inst.components.tradable.goldvalue = TUNING.GOLD_VALUES.MEAT
+
+    inst:AddComponent("perishable")
+    inst.components.perishable:SetPerishTime(TUNING.PERISH_PRESERVED)
+    inst.components.perishable:StartPerishing()
+    inst.components.perishable.onperishreplacement = "spoiled_food"
+
+    MakeHauntableLaunchAndPerish(inst)
+    inst:ListenForEvent("spawnedfromhaunt", OnSpawnedFromHaunt)
+
+    return inst
+end
+
+return Prefab("common/inventory/plantmeat_dried", driedplantmeat, assets, prefabs)

c503cddccc21c91f84e99df18737c3c5e4293620
Ryan Williams <ryan.williams.1990@gmail.com>
2022-07-31 22:18:22 -0700

    Update modmain.lua

--- Respawn Light/modmain.lua
+++ Respawn Light/modmain.lua
@@ -1,13 +1,14 @@
 
-if not GLOBAL.TheNet:GetIsServer() then
+local _G = GLOBAL
+if not _G.TheNet:GetIsServer() then
     return
 end
 
 AddPlayerPostInit(function(inst)
     inst:ListenForEvent("ms_respawnedfromghost", function(inst)
         local x, y, z = inst.Transform:GetWorldPosition()
-        if x and not GLOBAL.TheWorld.state.isday and #GLOBAL.TheSim:FindEntities(x, y, z, 4, {"spawnlight"}) <= 0 then
-            GLOBAL.SpawnPrefab("spawnlight_multiplayer").Transform:SetPosition(x, y, z)
+        if x and not _G.TheWorld.state.isday and #_G.TheSim:FindEntities(x, y, z, 4, {"spawnlight"}) <= 0 then
+            _G.SpawnPrefab("spawnlight_multiplayer").Transform:SetPosition(x, y, z)
         end
     end)
 end)

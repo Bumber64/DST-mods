af9eb63007de3d158348eb5e79898c430b4ddbf9
Ryan Williams <ryan.williams.1990@gmail.com>
2022-08-03 00:48:19 -0700

    Update modinfo.lua

--- Improved Mushroom Planters/modinfo.lua
+++ Improved Mushroom Planters/modinfo.lua
@@ -2,7 +2,7 @@
 name = "Improved Mushroom Planters"
 description = "Planted mushrooms don't turn into rot in winter, plus configurable options for fertilizers and moon shrooms."
 author = "Bumber"
-version = "1.1"
+version = "1.2"
 forumthread = ""
 
 icon_atlas = "modicon.xml"

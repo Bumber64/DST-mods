742f8006fc74b8f36af710ab652f763d13d165f0
Ryan Williams <ryan.williams.1990@gmail.com>
2022-08-22 18:47:47 -0700

    Update modinfo.lua

--- Sturdier Spider Den Decorations/modinfo.lua
+++ Sturdier Spider Den Decorations/modinfo.lua
@@ -1,14 +1,14 @@
 
 name = "Sturdier Spider Den Decorations"
-description = "Prevents spider dens from becoming undecorated when nearby spiders are attacked by monsters."
+description = "Prevents spider dens from becoming undecorated when nearby spiders are attacked by monsters, or when den is hit by earthquake debris."
 author = "Bumber"
-version = "1.4"
+version = "1.5"
 forumthread = ""
 
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 
-priority = -1000
+priority = -1
 
 api_version = 10
 dst_compatible = true

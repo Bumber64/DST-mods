c4b3a7f4dd4c5bf34493bf340fcf15abd5d2dd70
Ryan Williams <ryan.williams.1990@gmail.com>
2023-04-11 06:04:27 -0700

    Update modinfo.lua

--- Don't Fumble/modinfo.lua
+++ Don't Fumble/modinfo.lua
@@ -2,7 +2,7 @@
 name = "Don't Fumble"
 description = "Prevent players from fumbling tools and weapons. Also prevent monsters from smashing and stealing. Configurable."
 author = "Bumber"
-version = "1.3"
+version = "1.4"
 forumthread = ""
 
 icon_atlas = "modicon.xml"

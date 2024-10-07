54302dfc0dd7fb5d511f505bdfcad9dc9dbcf9c2
Ryan Williams <ryan.williams.1990@gmail.com>
2024-10-07 01:42:16 -0700

    Update/rename GHST mod

---
+++ Ghost Through Creatures/modinfo.lua
@@ -0,0 +1,14 @@
+
+name = "Ghost Through Creatures"
+description = "Allow player ghosts to fly through other creatures."
+author = "Bumber"
+version = "1.2"
+forumthread = ""
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+api_version = 10
+dst_compatible = true
+all_clients_require_mod = false
+client_only_mod = false

a24d0b66cea036206cd01d7c0d4d794b64029142
Ryan Williams <ryan.williams.1990@gmail.com>
2024-11-11 22:30:26 -0800

    Update modmain.lua

--- Ice Flingomatic Watering/modmain.lua
+++ Ice Flingomatic Watering/modmain.lua
@@ -4,86 +4,83 @@ if not _G.TheNet:GetIsServer() then
     return
 end
 
-local UpvalueHacker = require("tools/upvaluehacker") --Rezecib's upvalue hacker
-
 local function modprint(s)
     print("[Ice Flingomatic Watering] "..s)
 end
-local function insertifnotexist(t, s)
+
+local function insert_if_not_exist(t, s)
     if table.contains(t, s) then
         return false
     end
-
     table.insert(t, s)
     return true
 end
+
+local HackUtil = require("tools/hackutil")
 
 -------------------------------------------
 ---------------- Settings -----------------
 -------------------------------------------
 
-local SMART_TARGET_WITHER = GetModConfigData("smart_target_wither") --target witherables only when too hot
-local SMART_TARGET_CROPS = GetModConfigData("smart_target_crops") --target crops only when actually needed
-local TARGET_CENTER = GetModConfigData("target_center") --target center of tile once instead of each crop
-local WATER_PERCENT = GetModConfigData("water_percent") --water crops when tile moisture falls below this value
-local ADD_WETNESS_AMOUNT = GetModConfigData("add_wetness") --snowballs add this much wetness
+local SMART_TARGET_WITHER = GetModConfigData("smart_target_wither") --bool; target witherables only when too hot
+local SMART_TARGET_CROPS = GetModConfigData("smart_target_crops") --bool; target crops only when actually needed
+local TARGET_CENTER = GetModConfigData("target_center") --bool; target center of tile once instead of each crop
+local WATER_PERCENT = GetModConfigData("water_percent") --num; water crops when tile moisture falls below this value
+local ADD_WETNESS = GetModConfigData("add_wetness") --num; snowballs add this much wetness
+local NO_FREEZE_FFFLY = GetModConfigData("no_freeze_fffly") --bool; snowballs don't affect friendy fruit fly
 
 -------------------------------------------
 ---------- Find Existing Stuff ------------
 -------------------------------------------
 
-local my_NOTAGS
-local my_NONEMERGENCYTAGS
-local my_NONEMERGENCY_FIREONLY_TAGS
-local my_RegisterDetectedItem
+local NOTAGS
+local NONEMERGENCY_FIREONLY_TAGS
+local RegisterDetectedItem
+
+local my_NONEMERGENCYTAGS --add "farmplantstress"
 
 local function find_fdetector_upvalues(self)
-    if my_RegisterDetectedItem then
-        return true
+    if my_NONEMERGENCYTAGS then
+        return true --already succeeded
     end
 
-    modprint("Upvalue hacking Activate for old LookForFiresAndFirestarters...")
-    local old_LFFAF = UpvalueHacker.GetUpvalue(self.Activate, "LookForFiresAndFirestarters")
-
+    modprint("Upvalue hacking firedetector.Activate for required values.")
+    local old_LFFAF, err_msg = HackUtil.GetUpvalue(self.Activate, "LookForFiresAndFirestarters")
     if not old_LFFAF then
-        modprint("Old LookForFiresAndFirestarters not found in Activate!")
+        modprint("firedetector.Activate"..err_msg)
         return false
     end
 
-    modprint("Upvalue hacking old LookForFiresAndFirestarters for NOTAGS...")
-    my_NOTAGS = UpvalueHacker.GetUpvalue(old_LFFAF, "NOTAGS")
-    if not my_NOTAGS then
+    NOTAGS, err_msg = HackUtil.GetUpvalue(old_LFFAF, "NOTAGS")
+    if not NOTAGS then
         modprint("NOTAGS not found in old LookForFiresAndFirestarters! Using default.")
-        my_NOTAGS = {"FX", "NOCLICK", "DECOR", "INLIMBO", "burnt", "player", "monster"}
+        NOTAGS = {"FX", "NOCLICK", "DECOR", "INLIMBO", "burnt", "player", "monster"}
     end
 
-    modprint("Upvalue hacking old LookForFiresAndFirestarters for NONEMERGENCYTAGS...")
-    my_NONEMERGENCYTAGS = UpvalueHacker.GetUpvalue(old_LFFAF, "NONEMERGENCYTAGS")
-    if not my_NONEMERGENCYTAGS then
-        modprint("NONEMERGENCYTAGS not found in old LookForFiresAndFirestarters! Using default.")
-        my_NONEMERGENCYTAGS = {"witherable", "fire", "smolder", "farmplantstress"} --target farm crops
-    elseif insertifnotexist(my_NONEMERGENCYTAGS, "farmplantstress") then --target farm crops
-        modprint("Added \"farmplantstress\" tag to existing NONEMERGENCYTAGS.")
-    else
-        modprint("Found \"farmplantstress\" tag in existing NONEMERGENCYTAGS.")
-    end
-
-    modprint("Upvalue hacking old LookForFiresAndFirestarters for NONEMERGENCY_FIREONLY_TAGS...")
-    my_NONEMERGENCY_FIREONLY_TAGS = UpvalueHacker.GetUpvalue(old_LFFAF, "NONEMERGENCY_FIREONLY_TAGS")
-    if not my_NONEMERGENCY_FIREONLY_TAGS then
+    NONEMERGENCY_FIREONLY_TAGS, err_msg = HackUtil.GetUpvalue(old_LFFAF, "NONEMERGENCY_FIREONLY_TAGS")
+    if not NONEMERGENCY_FIREONLY_TAGS then
         modprint("NONEMERGENCY_FIREONLY_TAGS not found in old LookForFiresAndFirestarters! Using default.")
-        my_NONEMERGENCY_FIREONLY_TAGS = {"fire", "smolder"}
+        NONEMERGENCY_FIREONLY_TAGS = {"fire", "smolder"}
     end
 
-    modprint("Upvalue hacking old LookForFiresAndFirestarters for RegisterDetectedItem...")
-    my_RegisterDetectedItem = UpvalueHacker.GetUpvalue(old_LFFAF, "RegisterDetectedItem")
-    if not my_RegisterDetectedItem then
+    RegisterDetectedItem, err_msg = HackUtil.GetUpvalue(old_LFFAF, "RegisterDetectedItem")
+    if not RegisterDetectedItem then
         modprint("RegisterDetectedItem not found in old LookForFiresAndFirestarters! Using default.")
-        my_RegisterDetectedItem = function(inst, self, target)
+        RegisterDetectedItem = function(inst, self, target)
             self.detectedItems[target] = inst:DoTaskInTime(2, function(inst, self, target)
                 self.detectedItems[target] = nil
             end, self, target)
         end
+    end
+
+    my_NONEMERGENCYTAGS, err_msg = HackUtil.GetUpvalue(old_LFFAF, "NONEMERGENCYTAGS")
+    if not my_NONEMERGENCYTAGS then
+        modprint("NONEMERGENCYTAGS not found in old LookForFiresAndFirestarters! Using default.")
+        my_NONEMERGENCYTAGS = {"witherable", "fire", "smolder", "farmplantstress"} --target farm crops
+    elseif insert_if_not_exist(my_NONEMERGENCYTAGS, "farmplantstress") then --target farm crops
+        modprint("Added \"farmplantstress\" tag to existing NONEMERGENCYTAGS.")
+    else
+        modprint("Found \"farmplantstress\" tag in existing NONEMERGENCYTAGS.")
     end
 
     return true
@@ -109,6 +106,7 @@ local function SoilCoord(target) --returns a string unique to crop's map tile or
     if TARGET_CENTER and target and target:HasTag("farmplantstress") then
         return string.format("%d,%d", GetTileCoords(target))
     end
+    return nil
 end
 
 local is_point_dry --set to appropriate fn based on settings
@@ -123,14 +121,12 @@ if WATER_PERCENT > 0.0 and not SMART_TARGET_CROPS then --need to acquire _moistu
 
             function self:GetSoilMoistureAtPoint(x, y, z)
                 if _moisturegrid == nil then
-                    modprint("_moisturegrid undefined. Upvalue hacking farming_manager.OnSave for _moisturegrid...")
-                    _moisturegrid = UpvalueHacker.GetUpvalue(self.OnSave, "_moisturegrid")
+                    modprint("Upvalue hacking farming_manager.OnSave for \"_moisturegrid\".")
+                    _moisturegrid, err_msg = HackUtil.GetUpvalue(self.OnSave, "_moisturegrid")
 
-                    if _moisturegrid then
-                        modprint("Defined _moisturegrid.")
-                    else
-                        _moisturegrid = false
-                        modprint("_moisturegrid not found in farming_manager.OnSave!")
+                    if not _moisturegrid then
+                        _moisturegrid = false --don't retry
+                        modprint("farming_manager.OnSave"..err_msg)
                         _G.TheNet:SystemMessage("[Ice Flingomatic Watering] Failed to find \"_moisturegrid\" in farming_manager component!")
                     end
                 end
@@ -148,7 +144,7 @@ if WATER_PERCENT > 0.0 and not SMART_TARGET_CROPS then --need to acquire _moistu
     is_point_dry = function(x, y, z)
         return _G.TheWorld.components.farming_manager:GetSoilMoistureAtPoint(x, y, z) < WATER_PERCENT
     end
-else
+else --dry is 0 moisture
     is_point_dry = function(x, y, z)
         return not _G.TheWorld.components.farming_manager:IsSoilMoistAtPoint(x, y, z)
     end
@@ -158,7 +154,7 @@ end
 ------------- Changed Stuff ---------------
 -------------------------------------------
 
-local function CheckTargetScore(target) --added crops; smarter witherable handling
+local function my_CheckTargetScore(target) --added crops; smarter witherable handling
     if not target:IsValid() then
         return 0
     elseif target.components.burnable then
@@ -170,9 +166,9 @@ local function CheckTargetScore(target) --added crops; smarter witherable handli
     end
 
     local w = target.components.witherable
-    if w and not w:IsProtected() then
-        if w:CanWither() then
-            if not SMART_TARGET_WITHER or _G.TheWorld.state.temperature >= w.wither_temp then --option to skip if not hot
+    if w and (w.protect_to_time == nil or w.protect_to_time < _G.GetTime() + 5.0) then --unprotected or soon to be
+        if w:CanWither() then --not already withered
+            if not SMART_TARGET_WITHER or _G.TheWorld.state.temperature > w.wither_temp - 1.0 then --option to skip if not hot
                 return 8
             end
         elseif w:CanRejuvenate() then
@@ -194,18 +190,18 @@ local function CheckTargetScore(target) --added crops; smarter witherable handli
     return 0
 end
 
-local function LookForFiresAndFirestarters(inst, self, force) --allow targeting of soil instead of individual crops
-    if not force and inst.sg ~= nil and inst.sg:HasStateTag("busy") then
+local function my_LookForFiresAndFirestarters(inst, self, force) --allow targeting of soil instead of individual crops
+    if not force and inst.sg and inst.sg:HasStateTag("busy") then
         return
     end
     local x, y, z = inst.Transform:GetWorldPosition()
-    local ents = _G.TheSim:FindEntities(x, y, z, self.range, nil, my_NOTAGS, (self.fireOnly and my_NONEMERGENCY_FIREONLY_TAGS) or my_NONEMERGENCYTAGS)
+    local ents = _G.TheSim:FindEntities(x, y, z, self.range, nil, NOTAGS, (self.fireOnly and NONEMERGENCY_FIREONLY_TAGS) or my_NONEMERGENCYTAGS)
     local target = nil
     local targetscore = 0
-    for i, v in ipairs(ents) do
+    for _,v in ipairs(ents) do
         local sc = SoilCoord(v) --use center of target's tile when TARGET_CENTER
         if not self.detectedItems[v] and not (sc and self.detectedItems[sc]) then
-            local score, force = CheckTargetScore(v)
+            local score, force = my_CheckTargetScore(v)
             if force then
                 target = v
                 break
@@ -218,13 +214,13 @@ local function LookForFiresAndFirestarters(inst, self, force) --allow targeting 
     if target then
         local sc = SoilCoord(target)
         if sc then --use center of soil tile
-            my_RegisterDetectedItem(inst, self, sc) --register the string
+            RegisterDetectedItem(inst, self, sc) --register the string
             if self.onfindfire ~= nil then
                 local x, y, z = GetCenterCoords(target)
                 self.onfindfire(inst, _G.Point(x, 0, z))
             end
         else --use position of entity
-            my_RegisterDetectedItem(inst, self, target)
+            RegisterDetectedItem(inst, self, target)
             if self.onfindfire ~= nil then
                 self.onfindfire(inst, target:GetPosition())
             end
@@ -237,18 +233,20 @@ end
 -------------------------------------------
 
 AddComponentPostInit("firedetector", function(self)
-    if find_fdetector_upvalues(self) then
-        modprint("Replacing LookForFiresAndFirestarters in firedetector component.")
-        UpvalueHacker.SetUpvalue(self.Activate, LookForFiresAndFirestarters, "LookForFiresAndFirestarters")
-    else
-        _G.TheNet:SystemMessage("[Ice Flingomatic Watering] Failed to modify firedetector component!")
+    if not find_fdetector_upvalues(self) then
+        return
+    elseif not HackUtil.SetUpvalue(self.Activate, my_LookForFiresAndFirestarters, "LookForFiresAndFirestarters") then
+        modprint("firedetector.Activate -> LookForFiresAndFirestarters not found! (This shouldn't happen!)")
     end
 end)
 
 AddPrefabPostInit("firesuppressor", function(inst)
-    inst.components.wateryprotection.addwetness = ADD_WETNESS_AMOUNT
+    inst.components.wateryprotection.addwetness = ADD_WETNESS
 end)
 
 AddPrefabPostInit("snowball", function(inst)
-    inst.components.wateryprotection.addwetness = ADD_WETNESS_AMOUNT
+    inst.components.wateryprotection.addwetness = ADD_WETNESS
+    if NO_FREEZE_FFFLY then
+        inst.components.wateryprotection:AddIgnoreTag("friendlyfruitfly")
+    end
 end)

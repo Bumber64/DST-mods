9a7c1162720533718281ec5db2f813532221e8bb
Ryan Williams <ryan.williams.1990@gmail.com>
2025-09-09 16:17:49 -0700

    Update modinfo.lua

--- Leaf Jerky/modinfo.lua
+++ Leaf Jerky/modinfo.lua
@@ -2,7 +2,7 @@
 name = "Leaf Jerky"
 description = "Turn leafy meat into leaf jerky on the drying rack."
 author = "Bumber"
-version = "1.0"
+version = "1.1"
 forumthread = ""
 
 icon_atlas = "modicon.xml"
@@ -11,4 +11,4 @@ icon = "modicon.tex"
 api_version = 10
 dst_compatible = true
 all_clients_require_mod = true
-client_only_mod = false
+clients_only_mod = false
